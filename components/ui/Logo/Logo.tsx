c530b9d71437f52f6cfba7cddf2469ef47353eaf
NestleeQ <tlustiyar@yandex.com>
2026-01-12 17:22:35 +0300

    feat: create logo component

---
+++ components/ui/Logo/Logo.tsx
@@ -0,0 +1,31 @@
+import { Manrope } from 'next/font/google'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const manropeSans = Manrope({
+	variable: '--font-manrope-sans',
+	subsets: ['latin']
+})
+
+export default function Logo() {
+	return (
+		<Link
+			href='/'
+			className='flex items-center'
+		>
+			<div className='flex justify-center items-center w-11 h-10 bg-neutral-900 rounded-full'>
+				<Image
+					src='/logo.svg'
+					alt='GearLab logo'
+					width={24}
+					height={24}
+				/>
+			</div>
+			<p
+				className={`${manropeSans.variable} antialiased text-[20px] font-extrabold tracking-[-3.5%] ml-3 text-neutral-900`}
+			>
+				GearLab
+			</p>
+		</Link>
+	)
+}
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,20 @@
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
+	plugins: {
+		'@tailwindcss/postcss': {}
+	},
+	theme: {
+		extend: {
+			container: {
+				center: true,
+				screen: {
+					sm: '100%',
+					md: '100%',
+					lg: '1116px',
+					xl: '1116px'
+				}
+			}
+		}
+	}
+}
 
-export default config;
+export default config
