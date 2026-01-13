865461633ac39b34f6a22cb3c4a8dd84a28b8cf8
NestleeQ <tlustiyar@yandex.com>
2026-01-13 11:33:08 +0300

    feat: create-main-screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/layout/Header/Header'
+import Banner from '@/components/ui/Banner/Banner'
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
+			<Banner />
 		</div>
 	)
 }
